ed385d1b2e1e8093fb5e830fab264f4636fb2c51
tanmaye bhatia <tanmayebhatia@gmail.com>
2020-01-21 16:33:36 -0500

    Commit with photo capture from dark to light. Next capture video.

--- LightSensorTest.py
+++ LightSensorTest.py
@@ -1,17 +1,24 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 LIGHT_PIN = 11
 GPIO.setup(LIGHT_PIN, GPIO.IN)
 lOld = not GPIO.input(LIGHT_PIN)
 print('Starting up the LIGHT Module (click on STOP to exit)')
 time.sleep(0.5)
+
 while True:
   if GPIO.input(LIGHT_PIN) != lOld:
     if GPIO.input(LIGHT_PIN):
-      print ('\u263e')
+      print ('Dark/0v')
     else:
-      print ('\u263c') 
+      print ('Light/5v')
+      camera.capture('LightPic2.jpg')
   lOld = GPIO.input(LIGHT_PIN)
   time.sleep(0.2)
